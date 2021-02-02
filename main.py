a72370ed3159fd02801ecb697bde4d5e1b90409c
di.smirnov <di.smirnov@corp.vk.com>
2021-02-02 22:27:04 +0300

    refactor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ client = AdbClient(host="127.0.0.1", port=5037)
 class Ui_MainWindow(QtWidgets.QWidget):
     def setupUi(self):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(510, 412)
+        MainWindow.resize(500, 180)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.mainLayout = QtWidgets.QGridLayout(self.centralwidget)
@@ -19,12 +19,13 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.fileDrop = FileEdit(self.centralwidget)
         self.mainLayout.addWidget(self.fileDrop, 0, 0, 1, 1)
 
-        self.forceDevices = QtWidgets.QPushButton('Обновить устройства')
+        self.forceDevices = QtWidgets.QPushButton("Обновить")
         self.mainLayout.addWidget(self.forceDevices, 0, 1, 1, 1)
 
         self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
         self.mainLayout.addWidget(self.scrollArea, 1, 0, 1, 2)
         self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Maximum)
         self.scrollWidget = QtWidgets.QWidget()
         self.scrollLayout = QtWidgets.QGridLayout(self.scrollWidget)
         self.scrollWidget.setLayout(self.scrollLayout)
@@ -54,15 +55,19 @@ class Ui_MainWindow(QtWidgets.QWidget):
         connected_devices = client.devices()
         for device in connected_devices:
             try:
-                deviceName = QtWidgets.QLabel(
-                    device.get_properties().get('ro.product.manufacturer') + " " + device.get_properties().get(
-                        'ro.product.model'))
+                name = device.get_properties().get('ro.product.manufacturer') + " " + device.get_properties().get(
+                        'ro.product.model')
+                deviceName = QtWidgets.QLabel(name)
                 deviceVersion = QtWidgets.QLabel("Android " + device.get_properties().get('ro.build.version.release'))
             except RuntimeError:
                 self.drawDevices()
 
             installButton = QtWidgets.QPushButton("Установить")
-            installButton.clicked.connect(lambda state, target=device: self.install(target))
+            installButton.clicked.connect(lambda state, target=device: self.install(target, name))
+
+            deviceName.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+            deviceVersion.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
+            installButton.setSizePolicy(QtWidgets.QSizePolicy.Maximum, QtWidgets.QSizePolicy.Maximum)
 
             self.scrollLayout.addWidget(deviceName, 0, i, 1, 1)
             self.scrollLayout.addWidget(deviceVersion, 1, i, 1, 1)
@@ -72,14 +77,14 @@ class Ui_MainWindow(QtWidgets.QWidget):
     def getPath(self):
         return self.fileDrop.placeholderText()
 
-    def install(self, device):
-        self.errorLabel.setText('Начало установки')
+    def install(self, device, name):
         try:
             device.install(path=self.getPath(), reinstall=True)
         except Exception:
-            self.errorLabel.setText('Произошла ошибка')
+            self.errorLabel.setText('Произошла ошибка установки на ' + name)
+            return
 
-        self.errorLabel.setText('Установка завершена')
+        self.errorLabel.setText('Установка завершена на ' + name)
 
 
 if __name__ == "__main__":
