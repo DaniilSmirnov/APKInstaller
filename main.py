d922744d7f7d74344677198db242b85cd5f00c4a
di.smirnov <di.smirnov@corp.vk.com>
2021-02-02 12:41:16 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,139 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
+from ppadb.client import Client as AdbClient
+import sys
+
+client = AdbClient(host="127.0.0.1", port=5037)
+
+path = None
+oldDevices = None
+
+
+class Ui_MainWindow(QtWidgets.QWidget):
+    def setupUi(self):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(510, 412)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.mainLayout = QtWidgets.QGridLayout(self.centralwidget)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.fileDrop = FileEdit(self.centralwidget)
+        self.mainLayout.addWidget(self.fileDrop, 0, 0, 1, 1)
+
+        self.forceDevices = QtWidgets.QPushButton('Обновить устройства')
+        self.mainLayout.addWidget(self.forceDevices, 0, 1, 1, 1)
+
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.mainLayout.addWidget(self.scrollArea, 1, 0, 1, 2)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollWidget = QtWidgets.QWidget()
+        self.scrollLayout = QtWidgets.QGridLayout(self.scrollWidget)
+        self.scrollWidget.setLayout(self.scrollLayout)
+        self.scrollArea.setWidget(self.scrollWidget)
+
+        self.errorLabel = QtWidgets.QLabel('')
+        self.mainLayout.addWidget(self.errorLabel, 2, 0, 1, 1)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "APK Installer"))
+
+        self.forceDevices.clicked.connect(self.drawDevices)
+
+        self.drawDevices()
+
+    def drawDevices(self):
+
+        for i in range(self.scrollLayout.count()):
+            self.scrollLayout.itemAt(i).widget().deleteLater()
+
+        i = 0
+
+        connected_devices = client.devices()
+        for device in connected_devices:
+            try:
+                deviceName = QtWidgets.QLabel(
+                    device.get_properties().get('ro.product.manufacturer') + " " + device.get_properties().get(
+                        'ro.product.model'))
+                deviceVersion = QtWidgets.QLabel("Android " + device.get_properties().get('ro.build.version.release'))
+            except RuntimeError:
+                self.drawDevices()
+
+            installButton = QtWidgets.QPushButton("Установить")
+            installButton.clicked.connect(lambda state, target=device: install(target, getPath()))
+
+            self.scrollLayout.addWidget(deviceName, 0, i, 1, 1)
+            self.scrollLayout.addWidget(deviceVersion, 1, i, 1, 1)
+            self.scrollLayout.addWidget(installButton, 2, i, 1, 1)
+            i += 1
+
+
+def getPath():
+    global path
+    print(path)
+    return path
+
+
+def install(device, path):
+    ui.errorLabel.setText('Начало установки')
+
+    try:
+        device.install(path=path, reinstall=True)
+    except Exception as e:
+        ui.errorLabel.setText(str(e))
+
+    ui.errorLabel.setText('Установка завершена')
+
+
+class FileEdit(QtWidgets.QLineEdit):
+    def __init__(self, parent):
+        super(FileEdit, self).__init__(parent)
+
+        self.setDragEnabled(True)
+
+    def dragEnterEvent(self, event):
+        data = event.mimeData()
+        urls = data.urls()
+        if urls and urls[0].scheme() == 'file':
+            event.acceptProposedAction()
+
+    def dragMoveEvent(self, event):
+        data = event.mimeData()
+        urls = data.urls()
+        if urls and urls[0].scheme() == 'file':
+            event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        global path
+        data = event.mimeData()
+        url = data.urls()[0]
+        path = url.toLocalFile()
+        self.setPlaceholderText(str(path))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi()
+    MainWindow.show()
+
+
+def updateDevices():
+    global oldDevices
+    newDevices = client.devices()
+
+    if oldDevices != newDevices:
+        #ui.drawDevices()
+        oldDevices = newDevices
+
+
+timer = QTimer()
+timer.timeout.connect(updateDevices)
+timer.start(30)
+
+sys.exit(app.exec_())
